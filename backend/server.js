8a4b7e80f2583002e0a333a8530f5de861bdfdd7
HeitorPF <heitorpf12.hf@gmail.com>
2026-01-14 23:44:54 -0300

    json da listas de kanjis (jlpt e grade)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const JishoAPI = require('unofficial-jisho-api');
+const fs = require('fs');
 
 const app = express();
 const port = 3001;
@@ -24,6 +26,30 @@ app.get('/api/kanji/:character', async (req, res) => {
         res.status(500).json({ error: 'Erro ao buscar dados' });
     }
 });
+
+app.get('/api/kanji/:category/:level', async (req, res) => {
+    const category = req.params.category;
+    const level = req.params.level;
+    let nameArq = ''
+
+    if (category === 'jlpt') {
+        nameArq = `jlpt_${level}.json`;
+    } else if (category === 'grade') {
+        nameArq = `grade_${level}.json`;
+    } else {
+        return res.status(400).json({ erro: "Categoria inválida. Use 'jlpt' ou 'grade'." });
+    }
+
+    const caminhoArquivo = path.join(__dirname, 'src/data', nameArq);
+    console.log('camim', caminhoArquivo)
+    try {
+        const dados = fs.readFileSync(caminhoArquivo, 'utf8');
+        const json = JSON.parse(dados);
+        res.json(json);
+    } catch (erro) {
+        res.status(500).json({ erro: "Erro ao ler o arquivo de dados." });
+    }
+})
 
 app.listen(port, () => {
     console.log(`Servidor Backend rodando em http://localhost:${port}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,11 +15,26 @@ function App() {
       .catch(err => console.error("Erro:", err));
   };
 
+  function listKanjis(category, level){
+    fetch(`http://localhost:3001/api/kanji/${category}/${level}`)
+      .then(response => response.json())
+      .then(result => {
+        console.log(`Dados de ${category} encontrados: `, result)
+      })
+      .catch(err => console.log('Erro:', err))
+  }
+
   return (
     <>
       <div>
         <input type="text" id='inputKanji' onChange={(e) => setKanji(e.target.value)}/>
-        <button onClick={searchKanji}>Procurar</button>
+        <button onClick={() => searchKanji()}>Procurar</button>
+        <button onClick={() => listKanjis('grade', '1')}>grade 1</button>
+        <button onClick={() => listKanjis('grade', '2')}>grade 2</button>
+        <button onClick={() => listKanjis('grade', '3')}>grade 3</button>
+        <button onClick={() => listKanjis('grade', '4')}>grade 4</button>
+        <button onClick={() => listKanjis('grade', '5')}>grade 5</button>
+        <button onClick={() => listKanjis('grade', '6')}>grade 6</button>
         <h1>Meu Dicionário</h1>
         {data ? (
           <div>
